a5eb9a8a5b877cf067e151431c216e9e54ff6447
Prakash Sathiakumar <psathiakumar@apple.com>
2025-05-06 03:07:22 -0700

    Movile view changes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,14 +2,24 @@ import { AppBar, Box, Button, CssBaseline, IconButton, Toolbar } from '@mui/mate
 import MenuIcon from '@mui/icons-material/Menu';
 import DarkModeIcon from '@mui/icons-material/DarkMode';
 import { useEffect, useState } from 'react';
-
+import Drawer from '@mui/material/Drawer';
+import Divider from '@mui/material/Divider';
+import List from '@mui/material/List';
+import ListIcon from '@mui/icons-material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemText from '@mui/material/ListItemText';
 const navItems = ['about', 'skills', 'experience', 'resume']
+const drawerWidth = 240;
 
 function Header() {
     // const { mode } = parentToChild;
 
-    // const [mobileOpen, setMobileOpen] = useState<boolean>(false);
+    const [mobileOpen, setMobileOpen] = useState<boolean>(false);
     const [scrolled, setScrolled] = useState<boolean>(false);
+    const handleDrawerToggle = () => {
+        setMobileOpen((prevState) => !prevState);
+    };
     useEffect(() => {
         const handleScroll = () => {
             const navbar = document.getElementById("navigation");
@@ -36,15 +46,34 @@ function Header() {
             }
         }
     }
+    const drawer = (
+        <Box className="navigation-bar-responsive" onClick={handleDrawerToggle} sx={{ textAlign: 'center' }}>
+            <p className="mobile-menu-top"><ListIcon />Menu</p>
+            <Divider />
+            <List>
+                {navItems.map((item) => (
+                    <ListItem key={item} disablePadding>
+                        <ListItemButton sx={{ textAlign: 'center' }} onClick={() => scrollToSection(item)}>
+                            <ListItemText primary={item.toUpperCase()} />
+                        </ListItemButton>
+                    </ListItem>
+                ))}
+            </List>
+        </Box>
+    );
     return (
         <Box component="nav" sx={{ display: 'flex' }}>
             <CssBaseline />
             <AppBar component="nav" id="navigation" className={`navbar-fixed-top${scrolled ? ' scrolled' : ''}`}>
-                <Toolbar className='navigation'>
-                    <IconButton sx={{ mr: 2, display: { sm: 'none' } }}>
+                <Toolbar className='navigation-bar'>
+                    <IconButton
+                        color="inherit"
+                        aria-label="open drawer"
+                        edge="start"
+                        onClick={handleDrawerToggle}
+                        sx={{ mr: 2, display: { sm: 'none' } }}>
                         <MenuIcon />
                     </IconButton >
-                    <DarkModeIcon />
                     <Box sx={{ display: { xs: 'none', sm: 'block' } }}>
                         {navItems.map(item => (
                             <Button key={item} className="sections" onClick={() => scrollToSection(item)}>
@@ -54,6 +83,22 @@ function Header() {
                     </Box>
                 </Toolbar>
             </AppBar>
+            <nav>
+                <Drawer
+                    variant="temporary"
+                    open={mobileOpen}
+                    onClose={handleDrawerToggle}
+                    ModalProps={{
+                        keepMounted: true, // Better open performance on mobile.
+                    }}
+                    sx={{
+                        display: { xs: 'block', sm: 'none' },
+                        '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
+                    }}
+                >
+                    {drawer}
+                </Drawer>
+            </nav>
         </Box>
     )
 }
